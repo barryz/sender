1c275c2043f3d95222a27b01f3a318484c14e2a7
barryz <barryzxb@gmail.com>
2017-01-10 15:26:14 +0800

    display slack message's count

--- http/proc.go
+++ http/proc.go
@@ -9,7 +9,7 @@ import (
 func configProcRoutes() {
 
 	http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fmt.Sprintf("sms:%v, mail:%v", proc.GetSmsCount(), proc.GetMailCount())))
+		w.Write([]byte(fmt.Sprintf("{\"mail\": %d, \"sms\": %d, \"slack\": %d}", proc.GetSmsCount(), proc.GetMailCount(), proc.GetSlackCount())))
 	})
 
 }
